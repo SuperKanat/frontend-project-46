8509c9fa444d79ae2388637a23b1c99d74fdcbfb
kanat <vasilyev.yuriy@hexly.ru>
2023-05-04 19:39:32 +0300

    gendiff fixtures

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -2,6 +2,9 @@
 import { Command } from 'commander/esm.mjs';
 const program = new Command();
 program
-  .version('0.0.1', '-V, --version', 'output the version number')
-  .description('Compares two configuration files and shows a difference.');
+  .version('0.0.1', '-v, --version', 'output the version number')
+  .description('Compares two configuration files and shows a difference.')
+  .option('-f, --format <type>', 'output format' )
+  .argument('<filepath1>')
+  .argument('<filepath2>');
 program.parse();
