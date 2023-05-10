bbb9ae2e0404af6785b344d36396fd2f56a00f87
kanat <vasilyev.yuriy@hexly.ru>
2023-05-10 12:42:39 +0300

    smth

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,10 +1,13 @@
 #!/usr/bin/env node
-import { Command } from 'commander/esm.mjs';
+import { Command } from 'commander';
 const program = new Command();
 program
   .version('0.0.1', '-v, --version', 'output the version number')
   .description('Compares two configuration files and shows a difference.')
   .option('-f, --format <type>', 'output format' )
   .argument('<filepath1>')
-  .argument('<filepath2>');
+  .argument('<filepath2>')
+  .action((filepath1, filepath2) => {
+    console.log(filepath1, filepath2);
+  });
 program.parse();
