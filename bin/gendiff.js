12d228a2827d30e11f7fd3bcfdc37c840e14c2b5
Slavantino <yuriy.vasilev.7474@gmail.com>
2023-09-12 21:26:31 +0300

    2 step completed

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -6,5 +6,6 @@ const program = new Command();
 program
     .version('0.1.0')
     .description('Compares two configuration files and shows a difference.')
+    .option('-f, --format <type>', 'output format')
 
 program.parse(process.argv)
