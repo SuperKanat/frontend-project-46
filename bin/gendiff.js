840da921bd96d2d9a6ff7afe5ab16824480bfe1c
Slavantino <yuriy.vasilev.7474@gmail.com>
2023-09-17 20:24:34 +0300

    makefile

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -7,5 +7,6 @@ program
     .version('0.1.0')
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format <type>', 'output format')
+    .arguments('<filePath1> <filePath2> ')
 
 program.parse(process.argv)
