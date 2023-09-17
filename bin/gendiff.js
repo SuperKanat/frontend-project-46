ba6449c00dd0b5f9ac51f0865cc715c703692b23
Slavantino <yuriy.vasilev.7474@gmail.com>
2023-09-17 21:19:38 +0300

    step 3 done, objecthasOwn problem

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 import { Command } from 'commander';
+import gendiff from '../src/index.js';
 
 const program = new Command();
 
@@ -8,5 +9,9 @@ program
     .description('Compares two configuration files and shows a difference.')
     .option('-f, --format <type>', 'output format')
     .arguments('<filePath1> <filePath2> ')
+    .action((filePath1, filePath2) => {
+        console.log(gendiff(filePath1, filePath2));
+      })
+    
 
 program.parse(process.argv)
