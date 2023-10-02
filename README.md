819e15f99a0c38bc26577ef890dc078eaa13f4f2
Slavantino <yuriy.vasilev.7474@gmail.com>
2023-10-02 15:59:48 +0300

    step 4 done (test-coverage not work)

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 ### Hexlet tests and linter status:
 [![Actions Status](https://github.com/SuperKanat/frontend-project-46/workflows/hexlet-check/badge.svg)](https://github.com/SuperKanat/frontend-project-46/actions)
+
+### Codeclimate Maintainability
+[![Maintainability](https://api.codeclimate.com/v1/badges/0b3f416d41fe8b25a3e1/maintainability)](https://codeclimate.com/github/SuperKanat/frontend-project-46/maintainability)
+
+### Codeclimate Test coverage
+[![Test Coverage](https://api.codeclimate.com/v1/badges/0b3f416d41fe8b25a3e1/test_coverage)](https://codeclimate.com/github/SuperKanat/frontend-project-46/test_coverage)
--- src/index.js
+++ src/index.js
@@ -2,17 +2,21 @@ import { readFileSync } from 'node:fs';
 import path from 'node:path';
 import _ from 'lodash';
 
+const resolvePath = (filePath) => (filePath.includes('__fixtures__')
+  ? path.resolve(process.cwd(), filePath)
+  : path.resolve(process.cwd(), `__fixtures__/${filePath}`));
+
 const gendiff = (filePath1, filePath2) => {
-  const path1 = path.resolve(process.cwd(), filePath1);
-  const path2 = path.resolve(process.cwd(), filePath2);
+  const path1 = resolvePath(filePath1);
+  const path2 = resolvePath(filePath2);
 
-  const file1 = readFileSync(path1, 'utf-8')
-  const file2 = readFileSync(path2, 'utf-8')
+  const file1 = readFileSync(path1, 'utf-8');
+  const file2 = readFileSync(path2, 'utf-8');
 
-  const data1 = JSON.parse(file1)
-  const data2 = JSON.parse(file2)
+  const data1 = JSON.parse(file1);
+  const data2 = JSON.parse(file2);
 
-  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort()
+  const keys = _.union(Object.keys(data1), Object.keys(data2)).sort();
 
   const result = ['{'];
   for (let key of keys) {
